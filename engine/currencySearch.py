649fbf2ea854bf8f58323ecb94808bf14fe14435
Vera <verna0066@gmail.com>
2019-08-01 16:24:13 +0800

    '上傳12'

--- engine/currencySearch.py
+++ engine/currencySearch.py
@@ -1,6 +1,6 @@
 import twder
 
-def currencySearch(search)
+def currencySearch(search):
 	dollarTupe = twder.now_all()[search]
 	reply = '{}\n{} 的即期賣出價: {}'.format(dollarTuple[0],search,dollarTupe[4])
 	return reply
