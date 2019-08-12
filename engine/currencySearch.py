1cc058c6ea3e017821abd94205a2e4a556fba00b
Vera <verna0066@gmail.com>
2019-08-12 10:34:24 +0800

    test

--- engine/currencySearch.py
+++ engine/currencySearch.py
@@ -1,6 +1,6 @@
 import twder
 
 def currencySearch(search):
-	dollarTupe = twder.now_all()[search]
-	reply = '{}\n{} 的即期賣出價: {}'.format(dollarTuple[0],search,dollarTupe[4])
+	dollarTuple = twder.now_all()[search]
+	reply = '{}\n{} 的即期賣出價: {}'.format(dollarTuple[0],search,dollarTuple[4])
 	return reply
